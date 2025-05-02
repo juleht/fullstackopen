ad1645038e9e3b6e3c08b1dde8604f228dc7d30d
juslehto <32615813+juleht@users.noreply.github.com>
2025-05-02 14:25:22 +0300

    tehtava 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,8 +3,24 @@ import { useState } from 'react'
 const Header = ({ headers }) => <h1>{headers}</h1>
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-  
-const Display = ({ text, value }) => <div>{text} {value}</div>
+
+const Statistics = ( {feedback} ) => {
+  const all = feedback.good + feedback.neutral + feedback.bad
+  const average = (feedback.good + (feedback.bad * -1)) / all
+  const positive = (feedback.good / all) * 100
+
+return (
+  <div>
+    <table>
+      <tr><td>good {feedback.good}</td></tr>
+      <tr><td>neutral {feedback.neutral}</td></tr>
+      <tr><td>bad {feedback.bad}</td></tr>
+      <tr><td>all {all}</td></tr>
+      <tr><td>average {average}</td></tr>
+      <tr><td>positive {positive}</td></tr>
+    </table>
+  </div>
+)}
 
 
   
@@ -17,13 +33,6 @@ const App = () => {
     bad: 0
   })
 
-  const total = feedback.good + feedback.neutral + feedback.bad
-
-  const average = (feedback.good + (feedback.bad * -1)) / total
-
-  const positive = (feedback.good / total) * 100
-
-
   const handleGoodFeedbackClick = () => {setFeedback({ ...feedback, good: feedback.good + 1 })}
   const handleNeutralFeedbackClick = () => {setFeedback({ ...feedback, neutral: feedback.neutral + 1 })}
   const handleBadFeedbackClick = () => {setFeedback({ ...feedback, bad: feedback.bad + 1 })}
@@ -35,12 +44,7 @@ const App = () => {
       <Button onClick={handleNeutralFeedbackClick} text = 'neutral'/>
       <Button onClick={handleBadFeedbackClick} text = 'bad'/>
       <Header headers={headers[1]} />
-      <Display text='good' value={feedback.good} />
-      <Display text='neutral' value={feedback.neutral} />
-      <Display text='bad' value={feedback.bad} />
-      <Display text='all' value={total} />
-      <Display text='average' value={average} />
-      <Display text='positive' value={positive} />
+      <Statistics feedback={feedback}/>
     </div>
   )
 }
