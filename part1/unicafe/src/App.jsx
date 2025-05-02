2eb7fe912eec84800b4a733726735b1436f24bc5
juslehto <32615813+juleht@users.noreply.github.com>
2025-05-02 15:06:12 +0300

    tehtava 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,9 +4,17 @@ const Header = ({ headers }) => <h1>{headers}</h1>
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const StatisticsLine = ({ text, value}) => <div>{text} {value}</div>
+const StatisticsLine = ({ text, value, unit }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+      <td>{unit}</td>
+    </tr>
+  )
+}
 
-const Statistics = ( {feedback} ) => {
+const Statistics = ({ feedback }) => {
   const all = feedback.good + feedback.neutral + feedback.bad
   const average = (feedback.good + (feedback.bad * -1)) / all
   const positive = (feedback.good / all) * 100
@@ -17,40 +25,41 @@ const Statistics = ( {feedback} ) => {
     )
   }
   else
-  return (
-  <div>
-    <StatisticsLine text='good' value={feedback.good}/>
-    <StatisticsLine text='neutral' value={feedback.neutral}/>
-    <StatisticsLine text='bad' value={feedback.bad}/>
-    <StatisticsLine text='all' value={all}/>
-    <StatisticsLine text='average' value={average}/>
-    <StatisticsLine text='positive' value={positive}/>
-  </div>
-)}
+    return (
+      <table>
+        <tbody>
+          <StatisticsLine text='good' value={feedback.good} />
+          <StatisticsLine text='neutral' value={feedback.neutral} />
+          <StatisticsLine text='bad' value={feedback.bad} />
+          <StatisticsLine text='all' value={all} />
+          <StatisticsLine text='average' value={average.toFixed(1)} />
+          <StatisticsLine text='positive' value={positive.toFixed(1)} unit='%'/>
+        </tbody>
+      </table>
+    )
+}
 
-
-  
 const App = () => {
   const headers = ['give feedback', 'statistics']
 
   const [feedback, setFeedback] = useState({
-    good : 0,
+    good: 0,
     neutral: 0,
     bad: 0
   })
 
-  const handleGoodFeedbackClick = () => {setFeedback({ ...feedback, good: feedback.good + 1 })}
-  const handleNeutralFeedbackClick = () => {setFeedback({ ...feedback, neutral: feedback.neutral + 1 })}
-  const handleBadFeedbackClick = () => {setFeedback({ ...feedback, bad: feedback.bad + 1 })}
+  const handleGoodFeedbackClick = () => { setFeedback({ ...feedback, good: feedback.good + 1 }) }
+  const handleNeutralFeedbackClick = () => { setFeedback({ ...feedback, neutral: feedback.neutral + 1 }) }
+  const handleBadFeedbackClick = () => { setFeedback({ ...feedback, bad: feedback.bad + 1 }) }
 
   return (
     <div>
       <Header headers={headers[0]} />
-      <Button onClick={handleGoodFeedbackClick} text = 'good'/>
-      <Button onClick={handleNeutralFeedbackClick} text = 'neutral'/>
-      <Button onClick={handleBadFeedbackClick} text = 'bad'/>
+      <Button onClick={handleGoodFeedbackClick} text='good' />
+      <Button onClick={handleNeutralFeedbackClick} text='neutral' />
+      <Button onClick={handleBadFeedbackClick} text='bad' />
       <Header headers={headers[1]} />
-      <Statistics feedback={feedback}/>
+      <Statistics feedback={feedback} />
     </div>
   )
 }
