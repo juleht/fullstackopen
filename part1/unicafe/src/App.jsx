875fbda150d8cde23801900358a952fc94606a9b
juslehto <32615813+juleht@users.noreply.github.com>
2025-05-02 12:54:05 +0300

    refraktointia tehtavaan 1.5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,55 +1,38 @@
 import { useState } from 'react'
 
-const Header = ({headers}) => {
-  return (
-    <h1>{headers}</h1>
-  )
-}
+const Header = ({ headers }) => <h1>{headers}</h1>
 
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
-
-const Display = (props) => {
-  return (
-    <div>{props.text} {props.good} {props.neutral} {props.bad}</div>
-  )
-}
-
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+  
+const Display = ({ text, value }) => <div>{text} {value}</div>
+  
 
 
 const App = () => {
-  // otsikot
   const headers = ['give feedback', 'statistics']
-  
-  // tallenna napit omaan hook
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+
+  const [feedback, setFeedback] = useState({
+    good : 0,
+    neutral: 0,
+    bad: 0
+  })
+
+  const handleGoodFeedbackClick = () => {setFeedback({ ...feedback, good: feedback.good + 1 })}
+  const handleNeutralFeedbackClick = () => {setFeedback({ ...feedback, neutral: feedback.neutral + 1 })}
+  const handleBadFeedbackClick = () => {setFeedback({ ...feedback, bad: feedback.bad + 1 })}
+
+
 
   return (
     <div>
       <Header headers={headers[0]} />
-      <Button 
-      onClick={() => setGood(good + 1)} 
-      text = 'good'
-      />
-      <Button
-        onClick={() => setNeutral(neutral + 1)}
-        text = 'neutral'
-        />
-      <Button
-        onClick={() => setBad(bad + 1)}
-        text = 'bad'
-        />
+      <Button onClick={handleGoodFeedbackClick} text = 'good'/>
+      <Button onClick={handleNeutralFeedbackClick} text = 'neutral'/>
+      <Button onClick={handleBadFeedbackClick} text = 'bad'/>
       <Header headers={headers[1]} />
-      <Display text={'good'} good={good} />
-      <Display text={'neutral'} neutral={neutral} />
-      <Display text={'bad'} bad={bad} />
+      <Display text='good' value={feedback.good} />
+      <Display text='neutral' value={feedback.neutral} />
+      <Display text='bad' value={feedback.bad} />
     </div>
   )
 }
