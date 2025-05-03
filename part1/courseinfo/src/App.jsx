e7fb0466312c317545c289c63cf85f829e70ce36
juslehto <32615813+juleht@users.noreply.github.com>
2025-05-03 16:24:37 +0300

    tehtava 2.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,36 +1,6 @@
-const Header = ({ course }) => <h1>{course.name}</h1>
-
-const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
-    </div>
-  )
-}
-
-const Total = ({ course }) => {
-  const totalSum = course.parts.reduce((sum, exercise) => sum + exercise.exercises, 0)
-  return (
-    <b>total of {totalSum} exercises</b>
-  )
-}
+import Course from './Components/Course'
 
 
-const Course = ({ courses }) => {
-  return (
-    <div>
-      {courses.map(course => (
-        <div key={course.id}>
-          <Header course={course} />
-          <Content course={course} />
-          <Total course={course} />
-        </div>
-      ))}
-    </div>
-  )
-}
 
 const App = () => {
 
